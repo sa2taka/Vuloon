a1110239a321a60e58da05f2750fa80e4b9cffc8
sa2taka <sa2taka@gmail.com>
2022-01-23 04:38:47 +0900

    :bug: fix import

--- packages/@vuloon/app/src/renderer.tsx
+++ packages/@vuloon/app/src/renderer.tsx
@@ -1,4 +1,4 @@
-import { Layout } from '@/renderer/components/layout';
+import { Layout } from '@/renderer/components/Layout';
 import { Main } from '@/renderer/components/windows/Main';
 import React from 'react';
 import ReactDom from 'react-dom';
@@ -13,10 +13,6 @@ const renderProps = (): [React.ReactElement<any, any>, ReactDom.Container | null
     const child = <h1>unexpected</h1>;
     return [child, root];
   }
-};
-
-type Props = {
-  children: React.ReactElement<any, string | React.JSXElementConstructor<any>> & React.ReactNode;
 };
 
 const [child, root] = renderProps();
