a605548159471907bdb480dcbcca85ec845e224c
sa2taka <sa2taka@gmail.com>
2022-02-05 02:44:55 +0900

    :sparkles: add initial-page

--- packages/@vuloon/app/src/renderer.tsx
+++ packages/@vuloon/app/src/renderer.tsx
@@ -1,4 +1,5 @@
 import { Layout } from '@/renderer/components/Layout';
+import { InitialSetting } from '@/renderer/components/windows/InitialSetting';
 import { Main } from '@/renderer/components/windows/Main';
 import React from 'react';
 import ReactDom from 'react-dom';
@@ -8,6 +9,10 @@ const renderProps = (): [React.ReactElement<any, any>, ReactDom.Container | null
   if (document.getElementById('main-app')) {
     const root = document.getElementById('main-app');
     const child = <Main />;
+    return [child, root];
+  } else if (document.getElementById('initial-setting-app')) {
+    const root = document.getElementById('initial-setting-app');
+    const child = <InitialSetting />;
     return [child, root];
   } else {
     const root = document.body;
---
+++ packages/@vuloon/app/src/renderer/components/windows/InitialSetting.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const InitialSetting: React.VFC = () => {
+  return <div></div>;
+};
