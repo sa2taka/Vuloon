393befa078ad9d874d651f465507cd645aa7b29d
sa2taka <sa2taka@gmail.com>
2022-02-05 02:32:42 +0900

    :wrench: add language to config

--- packages/@vuloon/app/src/main/domain/entities/config.ts
+++ packages/@vuloon/app/src/main/domain/entities/config.ts
@@ -1,10 +1,12 @@
 import { themeKeys } from '@/main/domain/entities/theme';
+import { app } from 'electron';
 
 export interface Config {
   initial: boolean;
   caDir: string;
   proxyPort: number;
   theme: typeof themeKeys[number];
+  language: string;
 }
 
 export const defaultConfig = {
@@ -12,4 +14,5 @@ export const defaultConfig = {
   caDir: '',
   proxyPort: 5110,
   theme: 'light',
+  language: app.getLocale(),
 } as const;
