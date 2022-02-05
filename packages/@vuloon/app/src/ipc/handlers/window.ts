0df9af0f5dfcf4861b9da4bbfed954860e134606
sa2taka <sa2taka@gmail.com>
2022-02-05 15:16:27 +0900

    :sparkles: add to open onece initial

--- packages/@vuloon/app/src/ipc/handlers/window.ts
+++ packages/@vuloon/app/src/ipc/handlers/window.ts
@@ -1,10 +1,17 @@
 import { ipcMain } from 'electron';
 import { COMPLETE_INITIAL } from '../sendKeys';
 import { windowManager } from '@/main/domain/models/windowManager';
+import { getConfig, setConfig } from '@/main/domain/repositories/config';
 
 export const windowHandler = (): void => {
   ipcMain.on(COMPLETE_INITIAL, async () => {
     windowManager.quitInitialPage();
     windowManager.openIndexPage();
+    const config = getConfig();
+    const newConfig = {
+      ...config,
+      initial: false,
+    };
+    setConfig(newConfig);
   });
 };
